c221b1afa3b92ff856e00824b5ec2324a36acd6f
AidanJP <aidanpendlebury@gmail.com>
2020-10-02 18:57:56 +0100

    Initial Commit

---
+++ DogShow/DogShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogShowApp.swift
+//  DogShow
+//
+//  Created by Aidan Pendlebury on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DogShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
