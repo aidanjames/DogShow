26b2247ab0f7c2ecd4c9a6829044ff2aef7b0866
AidanJP <aidanpendlebury@gmail.com>
2020-10-18 19:45:01 +0100

    Added a navigation title and placeholder button for settings view.

--- DogShow/Views/ContentView.swift
+++ DogShow/Views/ContentView.swift
@@ -14,36 +14,46 @@ struct ContentView: View {
     @State private var showingReviewScreen = false
     
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(2...6, id: \.self) { i in
-                    if i.isMultiple(of: 2) {
-                        Button("\(i)") {
-                            viewModel.numberOfBreeds = i
+        NavigationView {
+            VStack {
+                HStack {
+                    ForEach(2...6, id: \.self) { i in
+                        if i.isMultiple(of: 2) {
+                            Button("\(i)") {
+                                viewModel.numberOfBreeds = i
+                            }
+                            .padding(.horizontal)
+                            .foregroundColor(viewModel.numberOfBreeds == i ? .red : .blue)
                         }
-                        .padding(.horizontal)
-                        .foregroundColor(viewModel.numberOfBreeds == i ? .red : .blue)
                     }
-                }                
-            }
-            Text("Score: \(viewModel.currentScore) / \(viewModel.dogsShown) (\(viewModel.percentCorrect)%)").padding()
+                }
+                Text("Score: \(viewModel.currentScore) / \(viewModel.dogsShown) (\(viewModel.percentCorrect)%)").padding()
                 
-            Button("Restart game") {
-                viewModel.restartGame()
-            }.padding()
-            
-            Spacer()
-            if let image = viewModel.image {
-                image.resizable().scaledToFit()
-            } else {
-                ProgressView()
+                Button("Restart game") {
+                    viewModel.restartGame()
+                }.padding()
+                
+                Spacer()
+                if let image = viewModel.image {
+                    image.resizable().scaledToFit()
+                } else {
+                    ProgressView()
+                }
+                Spacer()
+                ButtonView(viewModel: viewModel, showingReviewScreen: $showingReviewScreen)
+                    .padding()
             }
-            Spacer()
-            ButtonView(viewModel: viewModel, showingReviewScreen: $showingReviewScreen)
-                .padding()
-        }
-        .sheet(isPresented: $showingReviewScreen) {
-            ReviewQuizzView(viewModel: viewModel)
+            .sheet(isPresented: $showingReviewScreen) {
+                ReviewQuizzView(viewModel: viewModel)
+            }
+            .navigationTitle(Text("Dog show"))
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {} ) {
+                        Image(systemName: "gearshape")
+                    }
+                }
+            }
         }
     }
 }
